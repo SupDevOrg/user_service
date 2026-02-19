42b9bf3da035fd611bcd6696c27a85efdcb3a919
Ilia Lysov <illigiorno@gmail.com>
2026-02-20 02:53:04 +0400

    Small updates

--- src/main/java/ru/sup/userservice/repository/UserRepository.java
+++ src/main/java/ru/sup/userservice/repository/UserRepository.java
@@ -1,12 +1,8 @@
 package ru.sup.userservice.repository;
 
-import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.sup.userservice.entity.User;
 
@@ -18,13 +14,4 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     Page<User> findByUsernameStartingWithIgnoreCase(String username, Pageable pageable);
 
-    @Modifying
-    @Transactional
-    @Query("UPDATE User u SET u.email = :email WHERE u = :user")
-    void addEmailForUser(@Param("user") User user, @Param("email") String email);
-
-    @Modifying
-    @Transactional
-    @Query("UPDATE User u SET u.phone = :phone WHERE u = :user")
-    void addPhoneForUser(@Param("user") User user, @Param("phone") String phone);
 }
